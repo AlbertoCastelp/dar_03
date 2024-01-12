74a98ad954e6f88af056589a4e0d452bd47ee863
Alberto <albertocastellanoslimon@gmail.com>
2024-01-12 10:40:32 +0100

    se añade configuración para comprobar correo electronico en componente de inscripción

--- plataforma-aprendizaje/src/components/EnrollmentForm.js
+++ plataforma-aprendizaje/src/components/EnrollmentForm.js
@@ -1,14 +1,14 @@
 // src/components/EnrollmentForm.js
 import React, { useState } from 'react';
 
-// Componente funcional EnrollmentForm que representa un formulario de inscripción
 const EnrollmentForm = ({ onEnroll }) => {
-  // Estados para almacenar los datos del formulario
+  // Estados para los campos del formulario y el mensaje de error
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [error, setError] = useState('');
 
-  // Manejadores de cambio para actualizar los estados cuando se ingresan datos
+  // Funciones para manejar cambios en los campos
   const handleNameChange = (e) => {
     setName(e.target.value);
   };
@@ -21,24 +21,35 @@ const EnrollmentForm = ({ onEnroll }) => {
     setPhoneNumber(e.target.value);
   };
 
-  // Manejador para enviar el formulario al hacer clic en el botón "Inscribirse"
+  // Función para validar una dirección de correo electrónico con una expresión regular
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  // Función para manejar el envío del formulario
   const handleSubmit = () => {
-    // Validar los datos si es necesario antes de llamar a onEnroll
-    if (name.trim() === '' || email.trim() === '' || phoneNumber.trim() === '') {
-      alert('Por favor, completa todos los campos.');
+    // Validar los datos antes de llamar a onEnroll
+    if (name.trim() === '' || phoneNumber.trim() === '') {
+      setError('Por favor, completa todos los campos.');
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError('Por favor, ingresa una dirección de correo electrónico válida.');
       return;
     }
 
     // Llamar a la función onEnroll con los datos del formulario
     onEnroll({ name, email, phoneNumber });
 
-    // Limpiar los campos después de enviar el formulario
+    // Limpiar los campos después de enviar el formulario y eliminar cualquier mensaje de error
     setName('');
     setEmail('');
     setPhoneNumber('');
+    setError('');
   };
 
-  // JSX que representa el formulario de inscripción
   return (
     <div>
       <label htmlFor="name">Nombre:</label>
@@ -49,6 +60,9 @@ const EnrollmentForm = ({ onEnroll }) => {
 
       <label htmlFor="phoneNumber">Número de Teléfono:</label>
       <input type="tel" id="phoneNumber" value={phoneNumber} onChange={handlePhoneNumberChange} />
+
+      {/* Mostrar mensaje de error si existe */}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
 
       <button onClick={handleSubmit}>Inscribirse</button>
     </div>
